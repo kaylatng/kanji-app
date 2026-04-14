4105164c6ddf4b6701bbe3523a765c088f41e497
Kayla Nguyen <kthuyt08@gmail.com>
2026-04-13 22:52:58 -0700

    add react compiler

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/kanji-app/' : '/',
   plugins: [
     react(),
     tailwindcss(),
   ],
-  
-})
+}))
